7dfc12bdd5382051dd053ac053a300e8444c2895
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-16 10:34:23 +0200

    fix footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,8 @@ import Logo from "../Assets/Logo.svg";
 import { BsTwitter } from "react-icons/bs";
 import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
+import { FiMapPin } from "react-icons/fi";
+import { TbWorldWww } from "react-icons/tb";
 /* import { FaFacebookF } from "react-icons/fa";*/
 import { FaInstagram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
@@ -15,10 +17,10 @@ const Footer = () => {
                     <img src={Logo} alt="" />
                 </div>
                 <div className="footer-icons">
-                    <BsTwitter />
-                    <FaInstagram />
-                    <span><a className="enlacefooter" href="https://github.com/JoanSimonutti" target="_blank"><FaGithub /></a></span>
                     <span><a className="enlacefooter" href="https://www.linkedin.com/in/joansimonutti/" target="_blank"><SiLinkedin /></a></span>
+                    <span><a className="enlacefooter" href="https://github.com/JoanSimonutti" target="_blank"><FaGithub /></a></span>
+                    <span><a className="enlacefooter" href="https://theoldstorehouse.ie/" target="_blank"><TbWorldWww /></a></span>
+                    <span><a className="enlacefooter" href="https://www.google.com/maps/dir//3+Crown+Alley,+Temple+Bar,+Dublin,+D02+CX67,+Ireland/@53.3186908,-6.420619,10.86z/data=!4m8!4m7!1m0!1m5!1m1!1s0x48670e9cb4613089:0xa8e4636215b85741!2m2!1d-6.262646!2d53.345176?hl=en&entry=ttu&g_ep=EgoyMDI1MDUxMy4xIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D" target="_blank"><FiMapPin /></a></span>
                 </div>
             </div>
             <div className="footer-section-two">
