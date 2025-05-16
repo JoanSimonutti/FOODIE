413188b289ac3508518aba24e4e404db0fdeede3
Joan Simonutti <joansimonutticode@gmail.com>
2025-05-16 10:03:48 +0200

    fix footer links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,7 @@ import { SiLinkedin } from "react-icons/si";
 import { BsYoutube } from "react-icons/bs";
 /* import { FaFacebookF } from "react-icons/fa";*/
 import { FaInstagram } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -14,9 +15,10 @@ const Footer = () => {
                     <img src={Logo} alt="" />
                 </div>
                 <div className="footer-icons">
-                    <SiLinkedin />
                     <BsTwitter />
                     <FaInstagram />
+                    <span><a className="enlacefooter" href="https://github.com/JoanSimonutti" target="_blank"><FaGithub /></a></span>
+                    <span><a className="enlacefooter" href="https://www.linkedin.com/in/joansimonutti/" target="_blank"><SiLinkedin /></a></span>
                 </div>
             </div>
             <div className="footer-section-two">
@@ -29,13 +31,13 @@ const Footer = () => {
                 <div className="footer-section-columns">
                     <span></span>
                     <span></span>
-                    <span>Políticas de privacidad</span>
-                    <span>FOODIE Internacional</span>
+                    <span></span>
+                    <span></span>
                 </div>
                 <div className="footer-section-columns">
                     <span></span>
                     <span></span>
-                    <span><a className="enlacefooter" href="https://github.com/JoanSimonutti" target="_blank">JOAN SIMONUTTI</a></span>
+                    <span>Políticas de privacidad</span>
                     <span>FOODIE © 2025</span>
                 </div>
             </div>
